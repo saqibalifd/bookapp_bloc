c067303c5acdc6cdea540dbd495fd645ff40ead4
Saqib Ali <saqibali.fd@gmail.com>
2025-12-03 21:53:46 +0500

    data pushd

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,12 +1,35 @@
+import 'package:bookapp/bloc/book/book_bloc.dart';
 import 'package:bookapp/config/component/components.dart';
+import 'package:bookapp/dependencies_injection/locator.dart';
 import 'package:bookapp/l10n/app_localizations.dart';
 import 'package:bookapp/utils/extensions/general_extensions.dart';
 import 'package:bookapp/view/home/widgets/widgets.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  late BookBloc _bookBloc;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _bookBloc = BookBloc(booksApiRepository: getIt());
+    _bookBloc.add(BooksFetch());
+  }
+
+  @override
+  void dispose() {
+    _bookBloc.close();
+    super.dispose();
+  }
 
   final List<String> categoriesList = [
     'Novel',
@@ -15,53 +38,6 @@ class HomeView extends StatelessWidget {
     'Novel',
     'Stories',
     'School',
-  ];
-
-  final List<Map<String, dynamic>> bookList = [
-    {
-      'thumbnail':
-          'https://images-na.ssl-images-amazon.com/images/I/81bsw6fnUiL.jpg',
-      'name': 'Atomic Habits',
-      'author': 'James Clear',
-      'aboutAuthor':
-          'James Clear is a writer and speaker focused on habits, decision making, and continuous improvement.',
-      'overview':
-          'A practical guide to building good habits and breaking bad ones through small, consistent changes that compound over time.',
-      'price': '15.99',
-    },
-    {
-      'thumbnail':
-          'https://images-na.ssl-images-amazon.com/images/I/71aFt4+OTOL.jpg',
-      'name': 'The Alchemist',
-      'author': 'Paulo Coelho',
-      'aboutAuthor':
-          'Paulo Coelho is a Brazilian lyricist and novelist, best known for his novel *The Alchemist*, which has sold millions of copies worldwide.',
-      'overview':
-          'A story about Santiago, an Andalusian shepherd, who dreams of finding treasure and learns about the importance of following one’s dreams.',
-      'price': '12.49',
-    },
-    {
-      'thumbnail':
-          'https://images-na.ssl-images-amazon.com/images/I/81vpsIs58WL.jpg',
-      'name': 'Rich Dad Poor Dad',
-      'author': 'Robert T. Kiyosaki',
-      'aboutAuthor':
-          'Robert T. Kiyosaki is an entrepreneur, educator, and investor who is best known for challenging conventional views about money.',
-      'overview':
-          'A personal finance classic that contrasts the financial philosophies of two fathers—one rich and one poor—and teaches principles for building wealth.',
-      'price': '10.99',
-    },
-    {
-      'thumbnail':
-          'https://images-na.ssl-images-amazon.com/images/I/81eB+7+CkUL.jpg',
-      'name': 'The Subtle Art of Not Giving a F*ck',
-      'author': 'Mark Manson',
-      'aboutAuthor':
-          'Mark Manson is a blogger and bestselling author who writes about self-help, personal development, and life advice with humor and honesty.',
-      'overview':
-          'A counterintuitive approach to living a better life by focusing on what truly matters and letting go of superficial concerns.',
-      'price': '14.29',
-    },
   ];
 
   @override
@@ -75,29 +51,35 @@ class HomeView extends StatelessWidget {
           preferredSize: const Size.fromHeight(kToolbarHeight),
           child: const HomeAppbarWidget(),
         ),
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: context.mediaQueryWidth * .05,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(loc.wellcomeBackBunny, style: theme.textTheme.bodyMedium),
-                Text(
-                  loc.whatDoYouWantToRead,
-                  style: theme.textTheme.headlineLarge,
-                ),
-                20.height,
-                HomeSearchFieldWidget(),
-                20.height,
-                Expanded(
-                  child: HomeTabBarWidget(
-                    categoriesList: categoriesList,
-                    bookList: bookList,
+        body: BlocProvider(
+          create: (context) => _bookBloc,
+          child: SafeArea(
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: context.mediaQueryWidth * .05,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    loc.wellcomeBackBunny,
+                    style: theme.textTheme.bodyMedium,
                   ),
-                ),
-              ],
+                  Text(
+                    loc.whatDoYouWantToRead,
+                    style: theme.textTheme.headlineLarge,
+                  ),
+                  20.height,
+                  HomeSearchFieldWidget(),
+                  20.height,
+                  Expanded(
+                    child: HomeTabBarWidget(
+                      categoriesList: categoriesList,
+                      // bookList: bookList,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/view/home/widgets/home_tab_bar_widget.dart
+++ lib/view/home/widgets/home_tab_bar_widget.dart
@@ -1,14 +1,18 @@
+import 'package:bookapp/bloc/book/book_bloc.dart';
+import 'package:bookapp/config/component/loading_widget.dart';
 import 'package:bookapp/config/routes/route_names.dart';
+import 'package:bookapp/data/response/status.dart';
 import 'package:bookapp/utils/extensions/general_extensions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeTabBarWidget extends StatefulWidget {
   final List<String> categoriesList;
-  final List<Map<String, dynamic>> bookList;
+  // final List<Map<String, dynamic>> bookList;
   const HomeTabBarWidget({
     super.key,
     required this.categoriesList,
-    required this.bookList,
+    // required this.bookList,
   });
 
   @override
@@ -31,80 +35,108 @@ class _HomeTabBarWidgetState extends State<HomeTabBarWidget>
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Column(
-      children: [
-        TabBar(
-          controller: _tabController,
-          isScrollable: true,
-          tabs: widget.categoriesList
-              .map((category) => Tab(text: category))
-              .toList(),
-        ),
-        Expanded(
-          child: TabBarView(
-            controller: _tabController,
-            children: widget.categoriesList.map((category) {
-              final aspectRatio =
-                  (context.mediaQueryWidth / 2.4) /
-                  (context.mediaQueryHeight * 0.40);
+    return BlocBuilder<BookBloc, BookState>(
+      builder: (context, state) {
+        switch (state.booksList!.status) {
+          case Status.loading:
+            return const Center(child: LoadingWidget());
 
-              return GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: aspectRatio,
+          case Status.error:
+            return Center(child: Text(state.booksList!.message ?? "Error"));
+
+          case Status.completed:
+            final books = state.booksList!.data!.data;
+
+            if (books.isEmpty) {
+              return const Center(child: Text("Books list is empty"));
+            }
+
+            return Column(
+              children: [
+                TabBar(
+                  controller: _tabController,
+                  isScrollable: true,
+                  tabs: widget.categoriesList
+                      .map((category) => Tab(text: category))
+                      .toList(),
                 ),
-                physics: BouncingScrollPhysics(),
-                itemCount: widget.bookList.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: context.mediaQueryWidth * .03,
-                      vertical: context.mediaQueryHeight * .027,
-                    ),
-                    child: GestureDetector(
-                      onTap: () =>
-                          Navigator.pushNamed(context, RoutesName.detail),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadiusGeometry.circular(20),
-                            child: Image.network(
-                              widget.bookList[index]['thumbnail'],
-                              fit: BoxFit.cover,
-                              height: context.mediaQueryHeight * .3,
-                            ),
-                          ),
-                          Padding(
+                Expanded(
+                  child: TabBarView(
+                    controller: _tabController,
+                    children: widget.categoriesList.map((category) {
+                      final aspectRatio =
+                          (context.mediaQueryWidth / 2.4) /
+                          (context.mediaQueryHeight * 0.40);
+
+                      return GridView.builder(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          childAspectRatio: aspectRatio,
+                        ),
+                        physics: BouncingScrollPhysics(),
+                        itemCount: books.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
                             padding: EdgeInsets.symmetric(
-                              vertical: context.mediaQueryHeight * .01,
+                              horizontal: context.mediaQueryWidth * .03,
+                              vertical: context.mediaQueryHeight * .027,
                             ),
-                            child: Text(
-                              overflow: TextOverflow.ellipsis,
-                              widget.bookList[index]['name'],
-                              style: theme.textTheme.titleSmall!.copyWith(
-                                color: theme.colorScheme.onSurface,
-                                fontWeight: FontWeight.bold,
+                            child: GestureDetector(
+                              onTap: () => Navigator.pushNamed(
+                                context,
+                                RoutesName.detail,
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadiusGeometry.circular(
+                                      20,
+                                    ),
+                                    child: Image.network(
+                                      books[index].thumbnail,
+                                      fit: BoxFit.cover,
+                                      height: context.mediaQueryHeight * .3,
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(
+                                      vertical: context.mediaQueryHeight * .01,
+                                    ),
+                                    child: Text(
+                                      overflow: TextOverflow.ellipsis,
+                                      books[index].bookName,
+                                      style: theme.textTheme.titleSmall!
+                                          .copyWith(
+                                            color: theme.colorScheme.onSurface,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                    ),
+                                  ),
+                                  Text(
+                                    overflow: TextOverflow.ellipsis,
+                                    books[index].authorName,
+                                    style: theme.textTheme.bodyMedium!.copyWith(
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ],
                               ),
                             ),
-                          ),
-                          Text(
-                            overflow: TextOverflow.ellipsis,
-                            widget.bookList[index]['author'],
-                            style: theme.textTheme.bodyMedium!.copyWith(
-                              fontSize: 12,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              );
-            }).toList(),
-          ),
-        ),
-      ],
+                          );
+                        },
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ],
+            );
+          default:
+            return const SizedBox();
+        }
+      },
     );
+    ///////////////////////
+    // BlocBuilder<BookBloc, BookState>(builder: (context, state) {});
   }
 }
